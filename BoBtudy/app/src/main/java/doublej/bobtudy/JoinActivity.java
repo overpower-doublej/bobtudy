8b7139f011e07cd64858ebf5311839c9607fc9fa
tae-jun <i2r.jun@gmail.com>
2014-12-01 22:54:22 +0900

    Http 작업중~

--- BoBtudy/app/src/main/java/doublej/bobtudy/JoinActivity.java
+++ BoBtudy/app/src/main/java/doublej/bobtudy/JoinActivity.java
@@ -2,13 +2,21 @@ package doublej.bobtudy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.loopj.android.http.RequestParams;
+
+import doublej.bobtudy.http.UserHttp;
 
 
 public class JoinActivity extends Activity {
@@ -19,27 +27,50 @@ public class JoinActivity extends Activity {
 
     String[] items = {"apple", "lemon", "orange"};
 
+    private EditText editId;
+    private EditText editPwd1;
+    private EditText editPwd2;
+    private EditText editNickName;
+    private EditText editName;
+    private EditText editStuId;
+
+    private String _id;
+    private String pwd1;
+    private String pwd2;
+    private String nickName;
+    private String name;
+    private String stuId;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.join_main);
 
-        certifyID = (Button)findViewById(R.id.certifyID);
-        confirmJoin = (Button)findViewById(R.id.confirmJoin);
-        cancelJoin = (Button)findViewById(R.id.cancelJoin);
+        certifyID = (Button) findViewById(R.id.certifyID);
+        confirmJoin = (Button) findViewById(R.id.confirmJoin);
+        cancelJoin = (Button) findViewById(R.id.cancelJoin);
 
-        Spinner spin = (Spinner)findViewById(R.id.deptspinner);
+        editId = (EditText) findViewById(R.id.editId);
+        editPwd1 = (EditText) findViewById(R.id.editPwd1);
+        editPwd2 = (EditText) findViewById(R.id.editPwd2);
+        editNickName = (EditText) findViewById(R.id.editNickName);
+        editName = (EditText) findViewById(R.id.editName);
+        editStuId = (EditText) findViewById(R.id.editStuId);
+
+        Spinner spin = (Spinner) findViewById(R.id.deptspinner);
         spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                           @Override
-                                           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                                           }
+            }
 
-                                           @Override
-                                           public void onNothingSelected(AdapterView<?> adapterView) {
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-                                           }
-                                       });
+            }
+        });
 
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
@@ -58,7 +89,7 @@ public class JoinActivity extends Activity {
         confirmJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                RequestParams params = new RequestParams();
             }
         });
 
