acce6925e8b2d9afb90ab533d4810d242b6f212b
yog080787 <yog080787@gmail.com>
2014-12-05 22:58:24 +0900

    후헹 ㅎㅅㅎ

---
+++ BoBtudy/app/src/main/java/doublej/bobtudy/IconTextListAdapterBoBroomMember.java
@@ -0,0 +1,71 @@
+package doublej.bobtudy;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YeomJi on 2014. 12. 5..
+ */
+public class IconTextListAdapterBoBroomMember extends BaseAdapter {
+
+    private Context mContext;
+
+    private List<IconTextItemBoBroom> mItems = new ArrayList<IconTextItemBoBroom>();
+
+    public IconTextListAdapterBoBroomMember(Context context) {
+        mContext = context;
+    }
+
+    public void addItem(IconTextItemBoBroom it) {
+        mItems.add(it);
+    }
+
+    public void setListItems(List<IconTextItemBoBroom> lit) {
+        mItems = lit;
+    }
+
+    public int getCount() {
+        return mItems.size();
+    }
+
+    public Object getItem(int position) {
+        return mItems.get(position);
+    }
+
+    public boolean areAllItemsSelectable() {
+        return false;
+    }
+
+    public boolean isSelectable(int position) {
+        try {
+            return mItems.get(position).isSelectable();
+        } catch (IndexOutOfBoundsException ex) {
+            return false;
+        }
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        IconTextViewBoBroom itemView;
+        if (convertView == null) {
+            itemView = new IconTextViewBoBroom(mContext, mItems.get(position));
+        } else {
+            itemView = (IconTextViewBoBroom) convertView;
+
+            itemView.setIcon(mItems.get(position).getIcon());
+            itemView.setText(0, mItems.get(position).getData(0));
+            itemView.setText(1, mItems.get(position).getData(1));
+            itemView.setText(2, mItems.get(position).getData(2));
+        }
+
+        return itemView;
+    }
+}
