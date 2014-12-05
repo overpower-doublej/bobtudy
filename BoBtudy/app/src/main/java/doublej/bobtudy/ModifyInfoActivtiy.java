2c848dfb8c519b6d7ca70d4f440e987f4f0d1a00
yog080787 <yog080787@gmail.com>
2014-12-05 12:26:29 +0900

    aa

---
+++ BoBtudy/app/src/main/java/doublej/bobtudy/ModifyInfoActivtiy.java
@@ -0,0 +1,51 @@
+package doublej.bobtudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by YeomJi on 2014. 12. 5..
+ */
+public class ModifyInfoActivtiy extends Activity {
+
+    public static final int REQUEST_CODE_ANOTHER = 1001;
+
+    EditText modifyinfoNickName, modifyinfoPWOriginal, modifyinfoPWNew, modifyinfoPWNew2;
+    Button confirmModify, cancelModify;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modifyinfo_main);
+
+        confirmModify = (Button) findViewById(R.id.confirmModify);
+        cancelModify = (Button) findViewById(R.id.cancelModify);
+        modifyinfoNickName = (EditText) findViewById(R.id.modifyinfoNickName);
+        modifyinfoPWOriginal = (EditText) findViewById(R.id.modifyinfoPWOriginal);
+        modifyinfoPWNew = (EditText) findViewById(R.id.modifyinfoPWNew);
+        modifyinfoPWNew2 = (EditText) findViewById(R.id.modifyinfoPWNew2);
+
+        confirmModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+        cancelModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+    }
+}
---
+++ BoBtudy/app/src/main/java/doublej/bobtudy/VoteSuggestionActivity.java
@@ -0,0 +1,50 @@
+package doublej.bobtudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by YeomJi on 2014. 12. 5..
+ */
+public class VoteSuggestionActivity extends Activity {
+
+    public static final int REQUEST_CODE_ANOTHER = 1001;
+
+
+    EditText voteSuggestionTitle, voteSuggestionContent;
+    Button voteSuggestionOK, voteSuggestionCancel;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vote_suggestion);
+
+        voteSuggestionOK = (Button) findViewById(R.id.voteSuggestionOK);
+        voteSuggestionCancel = (Button) findViewById(R.id.voteSuggestionCancel);
+        voteSuggestionTitle = (EditText) findViewById(R.id.voteSuggestionTitle);
+        voteSuggestionContent = (EditText) findViewById(R.id.voteSuggestionContent);
+
+        voteSuggestionOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+        voteSuggestionCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+    }
+}
