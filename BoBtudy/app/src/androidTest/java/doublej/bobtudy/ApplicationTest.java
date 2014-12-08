e0c26412d6ce3870c709cab1a98ad45429baebd6
tae-jun <i2r.jun@gmail.com>
2014-12-08 13:20:56 +0900

    ㅇㅅㅇ

--- BoBtudy/app/src/androidTest/java/doublej/bobtudy/ApplicationTest.java
+++ BoBtudy/app/src/androidTest/java/doublej/bobtudy/ApplicationTest.java
@@ -3,11 +3,21 @@ package doublej.bobtudy;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+import doublej.bobtudy.http.UserHttp;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+
     }
 }
